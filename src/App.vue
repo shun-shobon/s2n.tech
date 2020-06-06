2f3a8ad6049b40db51787f6fe0870b8baf8a63f2
shun-shobon <shun.shobon@gmail.com>
2020-06-06 15:50:03 +0900

    feat: add title

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="app" />
+  <div id="app">
+    <Title />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import Title from "@/components/Title.vue";
+
 @Component({
-  name: "App"
+  name: "App",
+  components: { Title }
 })
 export default class App extends Vue {}
 </script>
+
+<style lang="scss">
+@import "~destyle.css";
+
+body {
+  background-color: black;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="title">
+    <div class="title">
+      <h1>SHUN</h1>
+      <h2>Shuntaro Nishizawa</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "Title"
+})
+export default class Title extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+#title {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .title {
+    color: white;
+    h1,
+    h2 {
+      text-align: center;
+    }
+    h1 {
+      font-size: 10rem;
+    }
+    h2 {
+      font-size: 3rem;
+    }
+  }
+}
+</style>
