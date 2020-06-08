4a28166567f73cd80ecb06560bc82817c97ebb75
shun-shobon <shun.shobon@gmail.com>
2020-06-08 23:15:40 +0900

    feat: add section title

---
+++ src/components/atoms/SectionTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="section-title">
+    <fa-icon class="section-title__icon" :icon="icon" />
+    <span class="section-title__text">{{ text }}</span>
+  </h1>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { Icon } from "@/utils/icon-type";
+
+@Component({
+  name: "SectionTitle"
+})
+export default class SectionTitle extends Vue {
+  @Prop({ required: true })
+  text!: string;
+
+  @Prop({ required: true })
+  icon!: Icon;
+}
+</script>
+
+<style lang="scss">
+.section-title {
+  text-align: center;
+  font-size: 3rem;
+
+  &__text {
+    margin-left: 1rem;
+  }
+}
+</style>
