3a5e51eb22a7fb1d3098699012bc1a1de3671525
shun-shobon <me@shun.technology>
2020-10-01 22:25:40 +0900

    refactor: replace hexagon inner div

--- src/components/atoms/Hexagon.tsx
+++ src/components/atoms/Hexagon.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from "react";
+import range from "../../utils/range";
 
 import styles from "./Hexagon.module.scss";
 
 const Hexagon: FC = () => (
   <div className={styles.hexagon}>
-    <div className={styles.hexagon__inner} />
-    <div className={styles.hexagon__inner} />
-    <div className={styles.hexagon__inner} />
+    {range({ stop: 3 }).map((i) => (
+      <div key={i} className={styles.hexagon__inner} />
+    ))}
   </div>
 );
 
