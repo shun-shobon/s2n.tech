9ec977c626e6b509ed90817c8529faca197569a1
shun-shobon <me@shun.technology>
2020-10-04 02:21:30 +0900

    feat: add click animation

--- src/components/atoms/HamburgerButton.tsx
+++ src/components/atoms/HamburgerButton.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import classNames from "classnames";
 import range from "../../utils/range";
 
 import styles from "./HamburgerButton.module.scss";
@@ -8,12 +9,20 @@ type Props = {
 };
 
 const HamburgerButton: FC<Props> = ({ onClick }) => {
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const hamburgerButtonClass = classNames(styles.hamburgerButton, {
+    [styles.active]: isOpen,
+  });
+
   const handleClick = () => {
+    setOpen(!isOpen);
+
     if (onClick) onClick();
   };
 
   return (
-    <div className={styles.hamburgerButton}>
+    <div className={hamburgerButtonClass}>
       <div className={styles.hamburgerButtonIcon} onClick={handleClick}>
         {range({ stop: 3 }).map((i) => (
           <span key={i} className={styles.hamburgerButtonIconBar} />
