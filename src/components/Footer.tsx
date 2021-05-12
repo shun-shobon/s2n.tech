da04b91f15acc87dfbda76453be9d896277417a2
Shuntaro Nishizawa <me@shun.technology>
2021-05-12 23:43:08 +0900

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import { h } from "preact";
+import type { FunctionComponent } from "preact";
+
+const Footer: FunctionComponent = ({ children }) => {
+  return <footer>{children}</footer>;
+};
+
+export default Footer;
