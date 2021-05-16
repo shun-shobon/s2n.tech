79cfc5c11a5eed41d07b370d52fd516e6c40800f
Shuntaro Nishizawa <me@shun.technology>
2021-05-16 14:27:12 +0900

    feat: add base url

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { minifyHtml } from "vite-plugin-html";
+import { minifyHtml, injectHtml } from "vite-plugin-html";
 
 export default {
   esbuild: {
     jsxFactory: "h",
     jsxFragment: "Fragment",
   },
-  plugins: [minifyHtml()],
+  plugins: [
+    minifyHtml(),
+    injectHtml({
+      injectData: {
+        baseUrl: process.env["BASE_URL"] ?? "https://shun.technology",
+      },
+    }),
+  ],
 };
