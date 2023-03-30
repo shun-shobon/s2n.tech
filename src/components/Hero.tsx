28ea8aaba5e33817bb398493d0dc97c9d75a9c2c
Shuntaro Nishizawa <me@shun.technology>
2023-03-31 03:08:25 +0900

    feat: Add comment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,15 @@
 import { css } from "@acab/ecsstatic";
+import iconSrc from "../assets/icon.jpg";
 
 export function Hero(): JSX.Element {
   return (
     <div className={hero}>
-      <div className={heroInner}></div>
+      <div className={heroInner}>
+        <h1 className={commentWrapper}>
+          <img src={iconSrc} className={icon} />
+          <div className={comment}>Hello, World!</div>
+        </h1>
+      </div>
     </div>
   );
 }
@@ -24,6 +30,8 @@ const heroInner = css`
     border: 2px solid #6c757d;
     border-radius: 2px;
     animation: hero-inner-in 0.75s ease-in-out;
+    display: grid;
+    place-items: center;
   }
 
   @keyframes hero-inner-in {
@@ -34,6 +42,74 @@ const heroInner = css`
     100% {
       opacity: 1;
       transform: scale(1);
+    }
+  }
+`;
+
+const commentWrapper = css`
+  @layer base {
+    display: flex;
+    align-items: center;
+    gap: 1em;
+    font-size: 2.5rem;
+
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+    }
+  }
+`;
+
+const icon = css`
+  @layer base {
+    width: 3em;
+    height: 3em;
+    border-radius: 50%;
+  }
+`;
+
+const comment = css`
+  @layer base {
+    padding: 0.5em;
+    border: 2px solid #6c757d;
+    border-radius: 8px;
+    position: relative;
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      border: 0.5em solid transparent;
+      top: 50%;
+      right: 100%;
+
+      @media screen and (max-width: 768px) {
+        top: unset;
+        right: unset;
+        bottom: 100%;
+        left: 50%;
+      }
+    }
+
+    &::before {
+      border-right-color: #6c757d;
+      transform: translateY(-50%);
+
+      @media screen and (max-width: 768px) {
+        border-right-color: transparent;
+        border-bottom-color: #6c757d;
+        transform: translateX(-50%);
+      }
+    }
+
+    &::after {
+      border-right-color: #fff;
+      transform: translateY(-50%) translateX(3px);
+
+      @media screen and (max-width: 768px) {
+        border-right-color: transparent;
+        border-bottom-color: #fff;
+        transform: translateX(-50%) translateY(3px);
+      }
     }
   }
 `;
