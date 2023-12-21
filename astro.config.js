9c10a7086eb1a3e290da1c00bcde9106fde37863
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-21 11:34:44 +0900

    build: Add vanilla-extract

--- astro.config.js
+++ astro.config.js
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [vanillaExtractPlugin()],
+	},
+});
