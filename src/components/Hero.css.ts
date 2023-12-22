467018612ebbdda41579c0ee177059101afc9abd
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-22 09:26:04 +0900

    feat: Add hero

---
+++ src/components/Hero.css.ts
@@ -0,0 +1,35 @@
+import { style } from "@vanilla-extract/css";
+
+import * as layers from "@/styles/layers.css";
+
+export const hero = style({
+	"@layer": {
+		[layers.components]: {
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+			gap: "16px",
+		},
+	},
+});
+
+export const icon = style({
+	"@layer": {
+		[layers.components]: {
+			width: "128px",
+			height: "128px",
+			borderRadius: "50%",
+			alignSelf: "start",
+		},
+	},
+});
+
+export const title = style({
+	"@layer": {
+		[layers.components]: {
+			fontSize: "1.5rem",
+			fontWeight: "bold",
+			lineHeight: "1.2",
+		},
+	},
+});
