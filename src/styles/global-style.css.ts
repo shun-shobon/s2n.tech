ddb0ab16aa0d44cb3536ac0a82ed820ae0f01d9c
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-25 01:32:51 +0900

    feat: Add works page

--- src/styles/global-style.css.ts
+++ src/styles/global-style.css.ts
@@ -4,4 +4,5 @@ globalStyle("html, body", {
 	fontFamily: "'Zen Kurenaido', sans-serif",
 	color: "#fff",
 	backgroundImage: "linear-gradient(to bottom, #000912, #001E30, #2D4556)",
+	minHeight: "100dvh",
 });
