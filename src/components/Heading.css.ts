714f781a286440f086331edef653b1e4ed9fc581
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-25 00:12:44 +0900

    feat: Add line-offset props

--- src/components/Heading.css.ts
+++ src/components/Heading.css.ts
@@ -1,4 +1,7 @@
-import { style } from "@vanilla-extract/css";
+import { createVar, style } from "@vanilla-extract/css";
+import { calc } from "@vanilla-extract/css-utils";
+
+export const lineOffset = createVar();
 
 export const heading = style({
 	fontSize: "4rem",
@@ -17,12 +20,12 @@ export const textWrapper = style({
 });
 
 export const text = style({
-	clipPath: "polygon(0.75lh 0, 100% 0, 100% 100%, 0 100%, 0 0.75lh)",
+	clipPath: `polygon(${lineOffset} 0, 100% 0, 100% 100%, 0 100%, 0 ${lineOffset})`,
 });
 
 export const line = style({
 	position: "absolute",
-	top: "0.75lh",
+	top: lineOffset,
 	left: 0,
 	width: "2em",
 	height: "2px",
@@ -34,7 +37,7 @@ export const line = style({
 
 export const star = style({
 	position: "absolute",
-	top: "calc(0.75lh - 5px)",
+	top: calc.subtract(lineOffset, "5px"),
 	left: "-5px",
 	width: "10px",
 	height: "10px",
