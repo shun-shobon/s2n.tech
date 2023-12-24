19cd4039074c242571945177e04865475f347400
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-25 01:51:27 +0900

    build: Add cloudflare pages intergration

--- astro.config.js
+++ astro.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
 	integrations: [compress()],
+	output: "hybrid",
+	adapter: cloudflare({
+		mode: "directory",
+	}),
 	vite: {
 		plugins: [vanillaExtractPlugin()],
 	},
