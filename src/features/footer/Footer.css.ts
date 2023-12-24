773eb9f64f0c61013185e23f2495e5c791ef4a1c
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-24 15:13:36 +0900

    feat: Add page footer

---
+++ src/features/footer/Footer.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const footer = style({
+	width: "100%",
+	borderTop: "1px solid #fff",
+	display: "flex",
+	justifyContent: "center",
+	padding: "16px",
+	marginTop: "16px",
+});
