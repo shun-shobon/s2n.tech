fb829886c9fc31c41bb9a6f239684c5dd011a1ea
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-25 18:19:37 +0900

    perf: Use inline svg instead of img tag

--- astro.config.js
+++ astro.config.js
@@ -3,10 +3,11 @@ import compress from "astro-compress";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import cloudflare from "@astrojs/cloudflare";
 import qwikdev from "@qwikdev/astro";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [compress(), qwikdev()],
+	integrations: [compress(), qwikdev(), icon()],
 	output: "hybrid",
 	adapter: cloudflare({
 		mode: "directory",
