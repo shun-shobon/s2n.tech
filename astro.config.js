c278abc7def7f2fa75f8d9325f678ae865de98ea
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-25 11:02:17 +0900

    build: Add Qwik

--- astro.config.js
+++ astro.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import cloudflare from "@astrojs/cloudflare";
+import qwikdev from "@qwikdev/astro";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [compress()],
-	output: "hybrid",
-	adapter: cloudflare({
-		mode: "directory",
-	}),
-	vite: {
-		plugins: [vanillaExtractPlugin()],
-	},
+  integrations: [compress(), qwikdev()],
+  output: "hybrid",
+  adapter: cloudflare({
+    mode: "directory"
+  }),
+  vite: {
+    plugins: [vanillaExtractPlugin()]
+  }
 });
