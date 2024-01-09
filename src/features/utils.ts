45a716fdc7b7192935e31d71a9294cd4a984827d
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-10 08:51:53 +0900

    refactor: Make more strictly for type-checking

--- src/features/utils.ts
+++ src/features/utils.ts
@@ -4,6 +4,14 @@ import { calendar } from "./locale";
 
 export function slugToYearMonth(slug: string): Temporal.PlainYearMonth {
 	const [year, month] = slug.split("-").slice(0, 2).map(Number);
+	if (
+		year === undefined ||
+		month === undefined ||
+		Number.isNaN(year) ||
+		Number.isNaN(month)
+	) {
+		throw new TypeError(`Invalid slug: ${slug}`);
+	}
 
 	return Temporal.PlainYearMonth.from({ year, month, calendar });
 }
