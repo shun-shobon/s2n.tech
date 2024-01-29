526cb5a8be9bf322196eea3b329a1379b08fac4c
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-29 17:42:49 +0530

    feat: astro-font - optimize fonts & prevent CLS

--- src/styles/global-style.css.ts
+++ src/styles/global-style.css.ts
@@ -3,7 +3,7 @@ import { globalStyle } from "@vanilla-extract/css";
 import { vars } from "./theme.css";
 
 globalStyle("html, body", {
-	fontFamily: "'Zen Kurenaido', sans-serif",
+	fontFamily: "var(--font-inter), sans-serif",
 	color: vars.color.text,
 	backgroundImage: "linear-gradient(to bottom, #000912, #001E30, #2D4556)",
 	minHeight: "100dvh",
