ca6198c5be22d02b787b363a52176184c1460891
NISHIZAWA Shuntaro <me@s2n.tech>
2025-07-12 15:32:39 +0900

    chore: prettier-plugin-tailwindcssの設定を修正

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,8 @@
 import { shun_shobon } from "@shun-shobon/style-guide/prettier";
 
-export default shun_shobon();
+export default shun_shobon(
+	{},
+	{
+		tailwindStylesheet: "./src/styles/global.css",
+	},
+);
